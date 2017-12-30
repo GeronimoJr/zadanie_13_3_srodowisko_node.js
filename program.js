4a177b2f30e6a7b7641623d4d44848b8bd0b071a
Arnold Sztandarski <a.sztandarski@wp.pl>
2017-12-30 15:30:04 +0100

    change

--- program.js
+++ program.js
@@ -5,7 +5,6 @@ console.log('/node info - informacje o wersji');
 console.log('/lang info - informacje o języku');
 
 process.stdin.on('readable', function() {
-    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
